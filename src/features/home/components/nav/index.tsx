88ebe150ab9dc1f53c122a91a9d97a9fb6e0be39
Ouattara Youssouf Emmanuel <ouattarayoussoufemmanuel@gmail.com>
2026-07-07 10:31:08 +0000

    Ajustements responsive de la navigation et des sections About/Services

--- src/features/home/components/nav/index.tsx
+++ src/features/home/components/nav/index.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils/utils";
 
 const Navbar = () => {
     return (
-        <nav className="w-full  p-8 flex items-center justify-between relative z-10">
+        <nav className="w-full px-4 py-6 lg:p-8 flex items-center justify-between relative z-10">
             <Logo />
 
             <div className="container-nav max-lg:hidden lg:flex-center relative z-100  ">
